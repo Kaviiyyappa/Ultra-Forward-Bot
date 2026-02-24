1e5fcc6fd134c58521bc290ea3dd7db0fc314208
Kaviiyyappa <93697664+Kaviiyyappa@users.noreply.github.com>
2026-02-24 15:26:12 +0530

    Update BOT_TOKEN and DATABASE_URI in config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "36464925")
     API_HASH = environ.get("API_HASH", "942f6440a3ab83321135d7c1927aba0a")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8454259213:AAHfg0CE0Em5ZrRB79ZcjUnY8Q1QpK0bFxM") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8570983388:AAGhLJIpUpslEH_iwRLaAP9r1QBuzeGND7s") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://for:for@cluster0.fgu4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://rarajan06_db_user:bMpXc2LLO8BIqWo0@cluster0.ice8cwo.mongodb.net/?appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "AutoForward")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8422190094').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003597680452'))
